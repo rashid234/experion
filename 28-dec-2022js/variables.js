38788b981c88f887d95fe4cded2e4804b30d92d4
rashid <rashidkaras23@gmail.com>
2022-12-28 19:06:28 +0530

    some changes

--- 28-dec-2022js/variables.js
+++ 28-dec-2022js/variables.js
@@ -2,13 +2,13 @@
     // if we use let the variables scope is in only that block
     // var is declared as global variable
 for (var i = 0; i < 5; i++){
-    console.log('in loop', i)
+    console.log('in loop', i);
 }
-console.log(i)
+console.log(i);
 
-const firstName = 'Aravi'; // if we use unchanged variables should use const keyword
+const firstName = 'Aravi'; // if we use unchanged variables should use const keyword 
 
-let data = null;// make the value of variable is null
+const data = null;// make the value of variable is null
 const data_base = 'js';
-console.log(data_base)
+console.log(data_base);
 
